d216c74f6f7a0eb697142d24eebc5191eeaeb384
Richard Feeney <richardfeeney7@gmail.com>
2019-12-03 01:22:53 +0000

    Created MusicDAO file

--- BigProject.py
+++ BigProject.py
@@ -1,41 +1,42 @@
 #!flask/bin/python
 from flask import Flask , jsonify, request, abort
-#from flask_cors import CORS
+from musicDAO  import musicDAO
+#from rockDAO import rockDAO
 
-app=Flask(__name__)
+app = Flask(__name__,static_url_path ="",static_folder="")
 
 # POP id is auto incrementing
 # Tested :  curl "http://127.0.0.1:5000/pop"
-popMusic=[
-    {"id": 1, "artist":"Ariana Grande","album":"Thank U, Next","price":1500},
-    {"id": 2, "artist":"Billie Eilish","album":"When We All Fall Asleep, Where Do We Go?","price":2500},
-    {"id": 3, "artist":"Shawn Mendes","album":"Shawn Mendes","price":1000},
-    {"id": 4, "artist":"Khalid","album":"Free Spirit","price":1799},
-    {"id": 5, "artist":"Post Malone","album":"Hollywood's Bleeding","price":1500}
-]
-nextPOPArtistId=6
+#popMusic=[
+#    {"id": 1, "artist":"Ariana Grande","album":"Thank U, Next","price":1500},
+#    {"id": 2, "artist":"Billie Eilish","album":"When We All Fall Asleep, Where Do We Go?","price":2500},
+#    {"id": 3, "artist":"Shawn Mendes","album":"Shawn Mendes","price":1000},
+#    {"id": 4, "artist":"Khalid","album":"Free Spirit","price":1799},
+#    {"id": 5, "artist":"Post Malone","album":"Hollywood's Bleeding","price":1500}
+#]
+#nextPOPArtistId=6
 
 # ROCK id is auto incrementing
 # Tested :  curl "http://127.0.0.1:5000/rock"
-rockMusic=[
-    {"id": 1, "artist":"Imagine Dragons","album":"Origins","price":1900},
-    {"id": 2, "artist":"The Beatles","album":"Abbey Road","price":2000},
-    {"id": 3, "artist":"Queen","album":"News of the World","price":1850},
-    {"id": 4, "artist":"Portugal. The Man","album":" The Man Woodstock","price":1099},
-    {"id": 5, "artist":"Foster The People","album":"Sacred Hearts Club","price":1100}
-]
-nextROCKArtistId=6
+#rockMusic=[
+#    {"id": 1, "artist":"Imagine Dragons","album":"Origins","price":1900},
+#    {"id": 2, "artist":"The Beatles","album":"Abbey Road","price":2000},
+#    {"id": 3, "artist":"Queen","album":"News of the World","price":1850},
+#    {"id": 4, "artist":"Portugal. The Man","album":" The Man Woodstock","price":1099},
+#    {"id": 5, "artist":"Foster The People","album":"Sacred Hearts Club","price":1100}
+#]
+#nextROCKArtistId=6
 
 # DISCO id is auto incrementing
 # Tested :  curl "http://127.0.0.1:5000/disco"
-discoMusic=[
-    {"id": 1, "artist":"Donna Summer","album":"Bad Girls","price":1350},
-    {"id": 2, "artist":"Bee Gees","album":"Saturday Night Fever","price":2050},
-    {"id": 3, "artist":"Village People","album":"Can't Stop the Music","price":2150},
-    {"id": 4, "artist":"Diana Ross","album":"Eaten Alive","price":1159},
-    {"id": 5, "artist":"Prince","album":"Purple Rain","price":1400}
-]
-nextDISCOArtistId=6
+#discoMusic=[
+#    {"id": 1, "artist":"Donna Summer","album":"Bad Girls","price":1350},
+#    {"id": 2, "artist":"Bee Gees","album":"Saturday Night Fever","price":2050},
+#    {"id": 3, "artist":"Village People","album":"Can't Stop the Music","price":2150},
+#    {"id": 4, "artist":"Diana Ross","album":"Eaten Alive","price":1159},
+#    {"id": 5, "artist":"Prince","album":"Purple Rain","price":1400}
+#]
+#nextDISCOArtistId=6
 
 
 #@app.route("/")
@@ -44,15 +45,18 @@ nextDISCOArtistId=6
 
 # Music Pop ( CURL test was successful : curl "http://127.0.0.1:5000/pop/1" )
 @app.route("/pop")
-def getAllPop():
-    return jsonify(popMusic)
+def getAllPOP():
+    results = musicDAO.getAllPOP()
+    return jsonify(results)
 
 @app.route("/pop/<int:id>")
 def findByIdPOP(id): # Tested : curl "http://127.0.0.1:5000/pop/1"
-    foundPOP = list(filter(lambda p: p['id'] == id, popMusic))
+   # foundPOP = list(filter(lambda p: p['id'] == id, popMusic))
+    foundPOP = musicDAO.findByIdPOP(id)
     if len(foundPOP) == 0:
         return jsonify ({}) , 204
-    return jsonify(foundPOP[0])
+    #return jsonify(foundPOP[0])
+    return jsonify(foundPOP)
 
 @app.route("/pop", methods=["POST"]) # Test :  curl -X POST "http://127.0.0.1:5000/pop"
 def createPOP():
@@ -67,7 +71,11 @@ def createPOP():
         "price": request.json["price"]
     }
     nextPOPArtistId += 1
-    popMusic.append(pop)
+    # popMusic.append(pop)
+    addPOP = (pop["artist"], pop["album"], pop["price"])
+    newID = popDAO.create(addPOP)
+    addPOP["id"] = newId
+
     return jsonify(popMusic) # Tested : curl -i -H "Content-Type:application/json" -X POST -d "{\"artist\":\"Richard\",\"album\":\"Test\",\"price\":1000}" "http://127.0.0.1:5000/pop"
 
 @app.route("/pop/<int:id>", methods=["PUT"]) # Tested : curl -X PUT "http://127.0.0.1:5000/pop/1"
@@ -100,17 +108,21 @@ def deletePOP(id):
 
 
 
-# Music Rock ( CURL test was successful : curl "http://127.0.0.1:5000/rock/1" )
+# Music Rock ( CURL test was successful : curl "http://127.0.0.1:5000/rock" )
 @app.route("/rock")
-def getAllRock():
-    return jsonify(rockMusic)
+def getAllROCK():
+    results= musicDAO.getAllROCK()
+    return jsonify(results)
+
 
 @app.route("/rock/<int:id>")
 def findByIdROCK(id): # Tested : curl "http://127.0.0.1:5000/rock/1"
-    foundROCK = list(filter(lambda r: r['id'] == id, rockMusic))
+    #foundROCK = list(filter(lambda r: r['id'] == id, rockMusic))
+    foundROCK = musicDAO.findByIdROCK(id)
     if len(foundROCK) == 0:
         return jsonify ({}) , 204
-    return jsonify(foundROCK[0])
+    #return jsonify(foundROCK[0])
+    return jsonify(foundROCK)
 
 
 @app.route("/rock", methods=["POST"]) # Test :  curl -X POST "http://127.0.0.1:5000/rock"
@@ -162,14 +174,16 @@ def deleteROCK(id):
 # Music Disco ( CURL test was successful : curl "http://127.0.0.1:5000/disco/1" )
 @app.route("/disco")
 def getAllDISCO():
-    return jsonify(discoMusic)
+    results = musicDAO.getAllDISCO()
+    return jsonify(results)
 
 @app.route("/disco/<int:id>")
 def findByIdDISCO(id): # # Tested : curl "http://127.0.0.1:5000/disco/1"
-    foundDISCO = list(filter(lambda d: d['id'] == id, discoMusic))
+    #foundDISCO = list(filter(lambda d: d['id'] == id, discoMusic))
+    foundDISCO = musicDAO.findByIdDISCO(id)
     if len(foundDISCO) == 0:
         return jsonify ({}) , 204
-    return jsonify(foundDISCO[0])
+    return jsonify(foundDISCO)
 
 @app.route("/disco", methods=["POST"]) # Test :  curl -X POST "http://127.0.0.1:5000/disco"
 def createDISCO():
---
+++ musicDAO.py
@@ -0,0 +1,175 @@
+import mysql.connector   #  pip3 install mysql-connector-python
+
+class musicDAO:
+  db=""
+  def __init__(self):
+    self.db = mysql.connector.connect( 
+    host="localhost",
+    user="root",
+    password="root",
+    database="music"
+    )
+
+# POP MUSIC SECTION
+  def getAllPOP(self):
+    cursor = self.db.cursor() 
+    sql="select * from popMusic" 
+    cursor.execute(sql)
+    results = cursor.fetchall() 
+    returnArray = []
+    for result in results:
+      returnArray.append(self.musicDict(result))
+    return returnArray
+
+  def findByIdPOP(self, id):
+    cursor = self.db.cursor()
+    sql="select * from popMusic where id = %s" 
+    values = (id,)
+    cursor.execute(sql, values) 
+    result = cursor.fetchone() 
+    return self.musicDict(result)
+
+
+  def createPOP(self, values):
+    cursor = self.db.cursor()
+    sql="insert into popMusic (artist, album, price) values (%s,%s,%s)" 
+    cursor.execute(sql, values)
+    self.db.commit() 
+    return cursor.lastrowid  
+
+
+  def updatePOP(self, values):
+    cursor = self.db.cursor()
+    sql="update popMusic set artist= %s, album=%s, price=%s where id = %s" 
+    cursor.execute(sql, values)
+    self.db.commit()
+
+
+  def deletePOP(self, id):
+    cursor = self.db.cursor()
+    sql="delete from popMusic where id = %s"
+    values = (id,) 
+    cursor.execute(sql, values)
+    self.db.commit() 
+
+
+  # convert pop result to a dictionary
+  def musicDict(self,result):
+    colnames = ["id", "artist","album","price"]
+    item ={}
+    # check if there is a result, otherwise return empty {}
+    if result:
+      for i, colName in enumerate(colnames):
+        value = result[i]
+        item[colName] = value
+    return item
+
+# Disco MUSIC SECTION
+  def getAllDISCO(self):
+      cursor = self.db.cursor() 
+      sql="select * from discoMusic" 
+      cursor.execute(sql)
+      results = cursor.fetchall() 
+      returnArray = []
+      for result in results:
+        returnArray.append(self.musicDict(result))
+      return returnArray
+
+  def findByIdDISCO(self, id):
+    cursor = self.db.cursor()
+    sql="select * from discoMusic where id = %s" 
+    values = (id,)
+    cursor.execute(sql, values) 
+    result = cursor.fetchone() 
+    return self.musicDict(result)
+
+
+  def createDISCO(self, values):
+    cursor = self.db.cursor()
+    sql="insert into discoMusic (artist, album, price) values (%s,%s,%s)" 
+    cursor.execute(sql, values)
+    self.db.commit() 
+    return cursor.lastrowid  
+
+
+  def updateDISCO(self, values):
+    cursor = self.db.cursor()
+    sql="update discoMusic set artist= %s, album=%s, price=%s where id = %s" 
+    cursor.execute(sql, values)
+    self.db.commit()
+
+
+  def deletedisco(self, id):
+    cursor = self.db.cursor()
+    sql="delete from discoMusic where id = %s"
+    values = (id,) 
+    cursor.execute(sql, values)
+    self.db.commit() 
+
+
+  # convert pop result to a dictionary
+  def musicDict(self,result):
+    colnames = ["id", "artist","album","price"]
+    item ={}
+    # check if there is a result, otherwise return empty {}
+    if result:
+      for i, colName in enumerate(colnames):
+        value = result[i]
+        item[colName] = value
+    return item
+
+# ROCK MUSIC SECTION
+  def getAllROCK(self):
+      cursor = self.db.cursor() 
+      sql="select * from rockMusic" 
+      cursor.execute(sql)
+      results = cursor.fetchall() 
+      returnArray = []
+      for result in results:
+        returnArray.append(self.musicDict(result))
+      return returnArray
+
+  def findByIdROCK(self, id):
+    cursor = self.db.cursor()
+    sql="select * from rockMusic where id = %s" 
+    values = (id,)
+    cursor.execute(sql, values) 
+    result = cursor.fetchone() 
+    return self.musicDict(result)
+
+
+  def createROCK(self, values):
+    cursor = self.db.cursor()
+    sql="insert into rockMusic (artist, album, price) values (%s,%s,%s)" 
+    cursor.execute(sql, values)
+    self.db.commit() 
+    return cursor.lastrowid  
+
+
+  def updateROCK(self, values):
+    cursor = self.db.cursor()
+    sql="update rockMusic set artist= %s, album=%s, price=%s where id = %s" 
+    cursor.execute(sql, values)
+    self.db.commit()
+
+
+  def deleteROCK(self, id):
+    cursor = self.db.cursor()
+    sql="delete from rockMusic where id = %s"
+    values = (id,) 
+    cursor.execute(sql, values)
+    self.db.commit() 
+
+
+  # convert pop result to a dictionary
+  def musicDict(self,result):
+    colnames = ["id", "artist","album","price"]
+    item ={}
+    # check if there is a result, otherwise return empty {}
+    if result:
+      for i, colName in enumerate(colnames):
+        value = result[i]
+        item[colName] = value
+    return item
+
+musicDAO = musicDAO()
